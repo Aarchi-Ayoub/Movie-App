8bf387cdfdd2d2192a820b7c889fb4d81750dfe0
Aarchi-Ayoub <ayoubaa084@gmail.com>
2022-10-07 15:18:34 +0100

    settings comp empty

---
+++ src/modules/Settings/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+// styles
+import {flexPage} from '../../common/styles/styles';
+
+export default props => {
+  return (
+    <View style={flexPage}>
+      <Text>Settings</Text>
+    </View>
+  );
+};
