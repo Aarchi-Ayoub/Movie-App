42c348e641a71ceb736b1211db7aa0c63f8638cc
Aarchi-Ayoub <ayoubaa084@gmail.com>
2022-10-07 16:28:26 +0100

    delete old code

--- src/index.tsx
+++ src/index.tsx
@@ -1,31 +1,11 @@
-import React, {useContext} from 'react';
-import {View, Text, Switch, StatusBar} from 'react-native';
-import AppThemeProvider, {AppThemeContext} from './provider/theme';
+import React from 'react';
+import AppThemeProvider from './provider/theme';
+import Router from './router';
 
 export default props => {
-  const {isDark, changeTheme, backgroundStyle, textColorStyle} =
-    useContext(AppThemeContext);
-
-  const toggleValue = (arg: boolean): void => {
-    changeTheme(arg);
-  };
-
   return (
     <AppThemeProvider>
-      <StatusBar
-        backgroundColor={'#5E8D48'}
-        translucent
-        barStyle={isDark ? 'light-content' : 'dark-content'}
-      />
-
-      <View style={backgroundStyle}>
-        <Text style={textColorStyle}>Main page</Text>
-        <Text style={textColorStyle}>
-          Mode {isDark ? 'Sombre' : 'Claire'} :
-        </Text>
-
-        <Switch value={isDark} onValueChange={toggleValue} />
-      </View>
+      <Router />
     </AppThemeProvider>
   );
 };
