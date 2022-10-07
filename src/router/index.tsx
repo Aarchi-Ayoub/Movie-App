a850ce7ccc43b7de45c7a75d7288b69a8f29d56d
Aarchi-Ayoub <ayoubaa084@gmail.com>
2022-10-07 15:18:45 +0100

    router file

---
+++ src/router/index.tsx
@@ -0,0 +1,25 @@
+// In App.js in a new project
+
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+// Screens
+import Home from '../modules/Home';
+import Settings from '../modules/Settings';
+
+const Stack = createNativeStackNavigator();
+
+export default () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+        initialRouteName="Home">
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Settings" component={Settings} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
