6a5f67c9e01ccec096746813018fb3efd530f26a
Aarchi-Ayoub <ayoubaa084@gmail.com>
2022-10-07 21:03:44 +0100

    edit code

--- src/modules/Home/index.tsx
+++ src/modules/Home/index.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {View, Text, Animated, Easing, TouchableOpacity} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 // styles
 import {flexPage} from '../../common/styles/styles';
 import {styles} from './styles';
+import {AppThemeContext} from '../../provider/theme';
 
 export default props => {
+  // Context values
+  const {backgroundStyle, isDark} = useContext(AppThemeContext);
+
   // Local variables
   const spinValue = new Animated.Value(0);
   const navigation = useNavigation();
@@ -26,14 +30,15 @@ export default props => {
     outputRange: ['0deg', '360deg'],
   });
 
+  // Navigation action
+  const navigate = (): void => navigation.navigate('Settings');
+
   return (
-    <View style={flexPage}>
+    <View style={[flexPage, backgroundStyle]}>
       <Text style={styles.title}>Home</Text>
       <View style={styles.content}>
         <View style={styles.raccourci}>
-          <TouchableOpacity
-            style={styles.settingTouch}
-            onPress={() => navigation.navigate('Settings')}>
+          <TouchableOpacity style={styles.settingTouch} onPress={navigate}>
             <Animated.Image
               style={[
                 styles.settingImg,
@@ -42,7 +47,11 @@ export default props => {
                 },
               ]}
               resizeMode="contain"
-              source={require('../../common/assets/settings.png')}
+              source={
+                isDark
+                  ? require('common/assets/settingsWhite.png')
+                  : require('common/assets/settings.png')
+              }
             />
           </TouchableOpacity>
         </View>
--- src/modules/Settings/index.tsx
+++ src/modules/Settings/index.tsx
@@ -1,12 +1,30 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useContext} from 'react';
+import {View, Text, Switch} from 'react-native';
 // styles
 import {flexPage} from '../../common/styles/styles';
+import {AppThemeContext} from '../../provider/theme';
 
 export default props => {
+  // Context values
+  const {isDark, changeTheme, backgroundStyle, textColorStyle} =
+    useContext(AppThemeContext);
+
+  // Switch actions
+  const toggleValue = (arg: boolean): void => {
+    changeTheme(arg);
+  };
+
   return (
     <View style={flexPage}>
-      <Text>Settings</Text>
+      <View style={[backgroundStyle, flexPage]}>
+        <Text style={textColorStyle}>Settings</Text>
+        <Text style={textColorStyle}>Main page</Text>
+        <Text style={textColorStyle}>
+          Mode {isDark ? 'Sombre' : 'Claire'} :
+        </Text>
+
+        <Switch value={isDark} onValueChange={toggleValue} />
+      </View>
     </View>
   );
 };
