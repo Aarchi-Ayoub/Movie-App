0111789fa230ed2ccfcfedefafc254a759841113
Aarchi-Ayoub <ayoubaa084@gmail.com>
2022-10-10 15:28:11 +0100

    Refetch on App focus

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {AppState, AppStateStatus, Platform} from 'react-native';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
-import {QueryClient, QueryClientProvider} from 'react-query';
+import {focusManager, QueryClient, QueryClientProvider} from 'react-query';
 import AppThemeProvider from './provider/theme';
 import Router from './router';
 
 export default props => {
   // Create a client
   const queryClient = new QueryClient();
+
+  const onAppStateChange = (status: AppStateStatus): void => {
+    if (Platform.OS !== 'web') {
+      focusManager.setFocused(status === 'active');
+    }
+  };
+
+  useEffect(() => {
+    const subscription = AppState.addEventListener('change', onAppStateChange);
+
+    return () => subscription.remove();
+  }, []);
 
   return (
     <SafeAreaProvider>
