094802bb1def64a21f4544dd2282f142ab4278ed
Aarchi-Ayoub <ayoubaa084@gmail.com>
2022-10-10 15:20:05 +0100

    react query provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {QueryClient, QueryClientProvider} from 'react-query';
 import AppThemeProvider from './provider/theme';
 import Router from './router';
 
 export default props => {
+  // Create a client
+  const queryClient = new QueryClient();
+
   return (
     <SafeAreaProvider>
-      <AppThemeProvider>
-        <Router />
-      </AppThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <AppThemeProvider>
+          <Router />
+        </AppThemeProvider>
+      </QueryClientProvider>
     </SafeAreaProvider>
   );
 };
