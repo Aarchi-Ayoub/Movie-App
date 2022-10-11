867cde5ff15a60f9832c09397ce11ee8612d9162
Aarchi-Ayoub <ayoubaa084@gmail.com>
2022-10-11 11:28:50 +0100

    hardcode api link

--- src/utils/interceptor.ts
+++ src/utils/interceptor.ts
@@ -4,7 +4,7 @@ import Config from 'react-native-config';
 const API = Config.API_URL || '';
 
 const client = axios.create({
-  baseURL: API,
+  baseURL: 'https://www.omdbapi.com',
 });
 
 const token = '';
@@ -13,6 +13,7 @@ export const request = ({...options}): Promise<Object> => {
   client.defaults.headers.common.Authorization = `Bearer ${token}`;
   const onSuccess = (response: any) => response;
   const onError = (error: any) => error;
+
   console.log('====================================');
   console.log('URL ==> ', API + options.url);
   console.log('====================================');
